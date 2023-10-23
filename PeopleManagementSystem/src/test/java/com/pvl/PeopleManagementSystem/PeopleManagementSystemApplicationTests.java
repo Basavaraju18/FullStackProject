4f55d015ac4bb29e36970e020d6f5b36ba6c42dd
Basavaraju18 <basavarajuds184@gmail.com>
2023-10-23 19:14:06 +0530

    Task is ready to lanch

---
+++ PeopleManagementSystem/src/test/java/com/pvl/PeopleManagementSystem/PeopleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvl.PeopleManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
